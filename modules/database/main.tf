6ef0d7d6cea9ef89c2eac92dfc1e873a2a704e09
Atul Manapragada <x2020gdf@stfx.ca>
2023-06-11 19:20:17 -0300

    changed sql server to mssql server

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,16 +1,21 @@
-resource "azurerm_sql_server" "primary" {
+resource "azurerm_mssql_server" "primary" {
     name = var.primary_database
     resource_group_name = var.resource_group
     location = var.location
     version = var.primary_database_version
     administrator_login = var.primary_database_admin
     administrator_login_password = var.primary_database_password
+    minimum_tls_version          = "1.2"
 }
 
-resource "azurerm_sql_database" "db" {
-  name                = "db"
-  resource_group_name = var.resource_group
-  location            = var.location
-  server_name         = azurerm_sql_server.primary.name
-}
 
+resource "azurerm_mssql_database" "db" {
+  name           = "db"
+  server_id      = azurerm_mssql_server.primary.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = true
+}
